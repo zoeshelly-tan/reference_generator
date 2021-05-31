fec0887e2a3a6cc667be8507a0b7297c24d9bc44
shelly-Tan <zoeshellytzl@gmail.com>
2021-05-31 21:42:19 +0930

    deploying

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,7 +8,7 @@ export default {
         //return axios.post("http://localhost:3001/login", userData)
         return axios({
             method: 'post',
-            url: 'http://localhost:3001/login',
+            url: '/login',
             data: userData
         });
 
